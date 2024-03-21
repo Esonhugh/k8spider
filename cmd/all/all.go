44b9c5e62ff4eeba2adb2e25c284c5d14f29c33a
tari <i@tari.moe>
2024-03-21 22:26:00 +0800

    feat: Support CoreDNS Wildcards feature of k8s < v1.25

--- cmd/all/all.go
+++ cmd/all/all.go
@@ -1,6 +1,7 @@
 package all
 
 import (
+	"github.com/esonhugh/k8spider/cmd/wildcard"
 	"net"
 	"os"
 
@@ -27,6 +28,7 @@ var AllCmd = &cobra.Command{
 			log.Warn("cidr is required")
 			return
 		}
+		wildcard.DumpWildCard(command.Opts.Zone)
 		records, err := scanner.DumpAXFR(dns.Fqdn(command.Opts.Zone), "ns.dns."+command.Opts.Zone+":53")
 		if err == nil {
 			printResult(records)
@@ -66,7 +68,7 @@ func RunBatch(net *net.IPNet) {
 
 func printResult(records define.Records) {
 	if command.Opts.OutputFile != "" {
-		f, err := os.OpenFile(command.Opts.OutputFile, os.O_CREATE|os.O_WRONLY, 0644)
+		f, err := os.OpenFile(command.Opts.OutputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Warnf("OpenFile failed: %v", err)
 		}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/esonhugh/k8spider/cmd/axfr"
 	_ "github.com/esonhugh/k8spider/cmd/service"
 	_ "github.com/esonhugh/k8spider/cmd/subnet"
+	_ "github.com/esonhugh/k8spider/cmd/wildcard"
 )
 
 func main() {

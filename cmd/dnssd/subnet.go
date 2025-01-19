9162404dca7850030baa64e721382eeed6554099
Esonhugh <esonhughoutside@gmail.com>
2025-01-19 12:31:29 +0800

    update: service to dnssd

--- cmd/dnssd/subnet.go
+++ cmd/dnssd/subnet.go
@@ -1,4 +1,4 @@
-package service
+package dnssd
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"github.com/esonhugh/k8spider/cmd"
 	_ "github.com/esonhugh/k8spider/cmd/all"
 	_ "github.com/esonhugh/k8spider/cmd/axfr"
+	_ "github.com/esonhugh/k8spider/cmd/dnssd"
 	_ "github.com/esonhugh/k8spider/cmd/dnsutils"
 	_ "github.com/esonhugh/k8spider/cmd/metrics"
 	_ "github.com/esonhugh/k8spider/cmd/neighbor"
-	_ "github.com/esonhugh/k8spider/cmd/service"
 	_ "github.com/esonhugh/k8spider/cmd/whereisdns"
 	_ "github.com/esonhugh/k8spider/cmd/wildcard"
 )

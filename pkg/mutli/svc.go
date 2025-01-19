0ff69b2be77d37a4050344003dbd981c68721786
Esonhugh <esonhughoutside@gmail.com>
2025-01-19 12:54:58 +0800

    update: piped record scanner

--- pkg/mutli/svc.go
+++ pkg/mutli/svc.go
@@ -3,15 +3,18 @@ package mutli
 import (
 	"github.com/esonhugh/k8spider/define"
 	"github.com/esonhugh/k8spider/pkg/scanner"
+	log "github.com/sirupsen/logrus"
 )
 
 func ScanServiceWithChan(rev <-chan define.Record) <-chan define.Record {
 	out := make(chan define.Record, 100)
 	go func() {
+		log.Tracef("piped scanning service port begin")
 		for records := range rev {
 			out <- scanner.ScanSingleSvcForPorts(records)
 		}
-		close(out)
+		// close(out)
+		log.Tracef("piped scanning service port ends")
 	}()
 	return out
 }
